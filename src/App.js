99bc4fd96a82c0e533db045a28249c0355e5a871
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-05-27 15:13:57 -0400

    board created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      game: {
+        board: []
+      }
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://minesweeper-api.herokuapp.com/games', {
+      method: 'POST',
+      body: JSON.stringify({ difficulty: 0 }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(resp => resp.json())
+      .then(newGame => {
+        console.log(newGame)
+        this.setState({
+          game: newGame
+        })
+      })
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Header />
+        <div>
+          <table>
+            <tbody>
+              {this.state.game.board.map((row, i) => {
+                return (
+                  <tr key={i}>
+                    {row.map((col, j) => {
+                      return (
+                        <td key={j} className="cell">
+                          {this.state.game.board[i][j]}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </>
+    )
   }
 }
 
